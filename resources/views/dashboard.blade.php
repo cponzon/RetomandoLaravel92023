8618f53886d3ba3eeff897bdf959fb02d381131a
cponzon <carlos.ponzon@gmail.com>
2023-02-03 14:09:39 -0500

    Actividades que puede realizar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,10 +9,39 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!")                        
+                    <!--{{("You're logged in!")                        
                      }}<br>
-                    {{ __("Ya estas logeado en la pagina, para que veas que sí busque hacia donde apuntaba, luego de hacer el registro o de logearte")
-                    }}
+                    {{("Ya estas logeado en la pagina, para que veas que sí busque hacia donde apuntaba, luego de hacer el registro o de logearte")
+                    }}-->
+                    <p>Hola __________, ya te encuentras logeado en nuestra página.
+                        Por favor selecciona la lista de actividades que deseas realizar:
+                    </p>
+                    <br>
+                    <h4>LISTA DE ACTIVIDADES</h4>
+                    <div class="large-4 columns" style="float: left">
+                        <input type="checkbox" name="Leer">
+                        <label title="tiene designado como actividad leer">Leer</label>
+                    </div>
+                    <div class="large columns-4" style="float: left">
+                        <input type="checkbox" name="Leer">
+                        <label title="tiene designado como actividad escribir">Escribir</label>
+                    </div>
+                    <div class="large columns-4" style="float: left">
+                        <input type="checkbox" name="Leer">
+                        <label title="tiene designado como actividad dibujar">Dibujar</label>
+                    </div>
+                    <div class="large columns-4" style="float: left">
+                        <input type="checkbox" name="Leer">
+                        <label title="tiene designado como actividad corregir">Corregir</label>
+                    </div>
+                    <div class="large columns-4" style="float: left">
+                        <input type="checkbox" name="Leer">
+                        <label title="tiene designado como actividad imprimir">Imprimir</label>
+                    </div>
+                    <div class="large columns-4" style="float: left">
+                        <input type="checkbox" name="Leer">
+                        <label title="tiene designado como actividad borrar">Borrar</label>
+                    </div>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function (){
     return view('welcome');
 });
 
+/*Route::get('/dashboard', LoginRegisterController::class);*/
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+/*Route::get('/', autorizacion::class);*/
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
