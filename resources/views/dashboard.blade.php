cbd6467027bbc49d2b15727e80b0892887b642bd
cponzon <carlos.ponzon@gmail.com>
2023-02-08 14:57:47 -0500

    wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,4 @@
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -5,6 +6,7 @@
         </h2>
     </x-slot>
 
+    <!--<link rel="stylesheet" href="{{ asset('css/stilo.css') }}" />-->
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -13,35 +15,54 @@
                      }}<br>
                     {{("Ya estas logeado en la pagina, para que veas que sí busque hacia donde apuntaba, luego de hacer el registro o de logearte")
                     }}-->
-                    <p>Hola __________, ya te encuentras logeado en nuestra página.
+                    <p>Hola , ya te encuentras logeado en nuestra página.
                         Por favor selecciona la lista de actividades que deseas realizar:
                     </p>
                     <br>
                     <h4>LISTA DE ACTIVIDADES</h4>
-                    <div class="large-4 columns" style="float: left">
-                        <input type="checkbox" name="Leer">
-                        <label title="tiene designado como actividad leer">Leer</label>
-                    </div>
-                    <div class="large columns-4" style="float: left">
-                        <input type="checkbox" name="Leer">
-                        <label title="tiene designado como actividad escribir">Escribir</label>
-                    </div>
-                    <div class="large columns-4" style="float: left">
-                        <input type="checkbox" name="Leer">
-                        <label title="tiene designado como actividad dibujar">Dibujar</label>
-                    </div>
-                    <div class="large columns-4" style="float: left">
-                        <input type="checkbox" name="Leer">
-                        <label title="tiene designado como actividad corregir">Corregir</label>
-                    </div>
-                    <div class="large columns-4" style="float: left">
-                        <input type="checkbox" name="Leer">
-                        <label title="tiene designado como actividad imprimir">Imprimir</label>
-                    </div>
-                    <div class="large columns-4" style="float: left">
-                        <input type="checkbox" name="Leer">
-                        <label title="tiene designado como actividad borrar">Borrar</label>
-                    </div>
+                    <form method="POST" action="/form-post">
+                    <table id="separacion_celdas">
+                        <tr>
+                            <td>
+                                <div class="large-4 columns" style="float: left">
+                                    <input type="checkbox" name="Leer">
+                                    <label title="tiene designado como actividad leer">Leer</label>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="large columns-4" style="float: left">
+                                    <input type="checkbox" name="Leer">
+                                    <label title="tiene designado como actividad escribir">Escribir</label>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="large columns-4" style="float: left">
+                                    <input type="checkbox" name="Leer">
+                                    <label title="tiene designado como actividad dibujar">Dibujar</label>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="large columns-4" style="float: left">
+                                    <input type="checkbox" name="Leer">
+                                    <label title="tiene designado como actividad corregir">Corregir</label>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="large columns-4" style="float: left">
+                                    <input type="checkbox" name="Leer">
+                                    <label title="tiene designado como actividad imprimir">Imprimir</label>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="large columns-4" style="float: left">
+                                    <input type="checkbox" name="Leer">
+                                    <label title="tiene designado como actividad borrar">Borrar</label>
+                                </div>
+                            </td>
+                        </tr>
+                    </table></form>
                 </div>
             </div>
         </div>
